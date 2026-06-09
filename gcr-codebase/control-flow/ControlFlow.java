d56eec3454c1774c8bc9268b52fb2fa211f3a7ac
ABHINAV_TIWARI <abhinav.tiwari_cs23@gla.ac.in>
2026-06-09 11:17:57 +0530

    control flow

---
+++ gcr-codebase/control-flow/ControlFlow.java
@@ -0,0 +1,17 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+
+
+        int k = 10;
+        int res1 = k++ + --k + k + k-- - ++k - --k;
+        System.out.println("Result: " + res1);
+
+
+        
+        
+
+
+        
+    }
+
+}
